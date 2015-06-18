f58243bc55c92fa0ecf6c8df7eb968c2886f82fc
qianfeng001 <qianfeng001@xiabowen.local>
2015-06-18 14:39:15 +0800

    Initial Commit

---
+++ 打印最大连续递减数字/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  打印最大连续递减数字
+//
+//  Created by qianfeng001 on 15/6/18.
+//  Copyright (c) 2015年 伟. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
